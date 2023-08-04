6e26b5d6960c3ddfdeba88789767132f8774124a
theit0 <theopelegrina469@gmail.com>
2023-08-04 09:38:48 -0300

    sprint almost finished

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,25 +8,31 @@ import img6 from '../assets/camels-resting.jpg'
 import img7 from '../assets/eiffel-tower-on-cloudy-day.jpg'
 import img8 from '../assets/hike-by-a-smoky-river.jpg'
 import img9 from '../assets/apartments-overlooking-a-canal.jpg'
+import img10 from '../assets/cliff-watcher.jpg'
+import img11 from '../assets/golden-thailand-canyon-sunset.jpg'
+import img12 from '../assets/roman-holiday.jpg'
 import Card from './Card'
-import ArrowLeft from '../assets/chevron-left.svg'
-import ArrowRight from '../assets/chevron-right.svg'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick'
+
 
 
 const Carousel = () => {
     const images = [
         {img: img1 , content:'Tokyo, Japan'},
         {img: img2 , content:'New York, USA'},
-        {img: img4 , content:'London, UK'},
+        {img: img4 , content:'London, England'},
         {img: img5 , content:'Seoul, South Korea'},
         {img: img6 , content:'Nueva Delhi, India'},
         {img: img3 , content:'Baku, Azerbaijan'},
         {img: img7 , content:'Paris, France'},
         {img: img8 , content:'Buenos Aires, Argentina'},
         {img: img9 , content:'Vienna, Austria'},
+        {img: img10 , content:'Havana, Cuba'},
+        {img: img11 , content:'Bangkok, Thailand'},
+        {img: img12 , content:'Roma, Italy'}
+
     ]
     const settings = {
       dots: true,
@@ -37,7 +43,7 @@ const Carousel = () => {
       initialSlide: 0,
       responsive: [
         {
-          breakpoint: 1400,
+          breakpoint: 1920,
           settings: {
             slidesToShow: 3,
             slidesToScroll: 3,
@@ -46,7 +52,7 @@ const Carousel = () => {
           }
         },
         {
-          breakpoint: 1050,
+          breakpoint: 1450,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
@@ -54,7 +60,7 @@ const Carousel = () => {
           }
         },
         {
-          breakpoint: 700,
+          breakpoint: 1000,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
