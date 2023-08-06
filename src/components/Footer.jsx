33e205585c89701354f2b1d6075252fd7657f95e
theit0 <theopelegrina469@gmail.com>
2023-08-05 22:27:21 -0300

    footer finished and responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,25 @@
 import '../styled-components/Footer.css'
 import Link from '../components/Link'
+import Facebook from '../assets/facebook.svg'
+import Instagram from '../assets/instagram.svg'
+import Tiktok from '../assets/tiktok.svg'
+import Youtube from '../assets/youtube.svg'
 
 export function Footer() {
   return(
       <footer>
 
           <div className='footer-links'>
-              <Link content='Terms of service'/>
-              <Link content='Privacy Policy'/>  
-              <Link content='Sitemap'/>
+              <Link content='Terms of service' href='/terms-of-service'/>
+              <Link content='Privacy Policy' href='/privacy-policy'/>
+              <Link content='Sitemap' href='/sitemap'/>
           </div>
 
           <div className='footer-socials'>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+              <a  href='https://www.youtube.com/'><img src={Facebook}/></a>
+              <a  href='https://www.facebook.com/'><img src={Instagram}/></a>
+              <a  href='https://www.instagram.com/'><img src={Tiktok}/></a>
+              <a  href='https://www.tiktok.com/'><img src={Youtube}/></a>
           </div>
 
       </footer>
