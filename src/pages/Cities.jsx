468dca0447134719b8b493a9a432a1b8bcd36327
theit0 <theopelegrina469@gmail.com>
2023-08-14 14:18:59 -0300

    filter working

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -5,11 +5,14 @@ import axios from 'axios';
 import Title from '../components/Title'
 import CityCard from '../components/CityCard';
 import zoom from '../assets/zoom-filled.svg'
+import Button from '../components/Button';
 
 const Cities = () => {
 
+  const [searchValue,setSearchValue] = useState(null)
   const [data,setData] = useState([]);
-  
+  const [found,setFound] = useState(true)
+
   useEffect(() => {
       const configs = {
         headers: {
@@ -19,30 +22,53 @@ const Cities = () => {
       axios.get('http://localhost:3000/api/cities',configs)
       .then(response => {
         setData(response.data.cities)
+        console.log(response.data.cities)
       })
       .catch(err => {
         console.log(err)
       })
   },[]);
 
+  const getCities = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/cities', {
+        params: { name: searchValue },
+      });
+      setData(response.data.cities)
+      setFound(true)
+    } catch (error) {
+      setFound(false)
+    }
+  };
 
   return (
     <div className='cities-container'>
       <div className='cities-title-container'>
         <Title content='Cities'/>
         <div className='search-container'>
-          <input type='text' className='search-input' placeholder='SEARCH FOR CITIES&#128269;'/>
+        <input
+          type='text'
+          className='search-input'
+          placeholder='SEARCH FOR CITIES&#128269;'
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+          <button  onClick={() => getCities()}>Filter</button>
         </div>
       </div>
-
       <div className='cities-cards-container'>
-          
-          {
+          { found &&
             data.map((city,index) => {
               return(
                 <CityCard img={city.img} content={`${city.name}, ${city.country}`} id={index}/>
               )
-            })
+            }) 
+          }
+          {
+            !found &&
+            <div className='not-found-container'>
+              <span>{`No se encontro ninguna ciudad con el nombre "${searchValue}".`}</span>
+            </div>
           }
       </div>
     </div>
