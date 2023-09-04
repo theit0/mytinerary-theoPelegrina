b4c7877baa4cdc1ce92a779093f424e6846ee462
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2023-09-04 01:45:45 -0300

    activities underconstruction added

---
+++ src/components/Activity.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styled-components/Activity.css'
+
+const Activity = () => {
+  return (
+    <div className='activity-container'>*Activities under construction*</div>
+  )
+}
+
+export default Activity
--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -1,8 +1,11 @@
 import '../styled-components/Itinerary.css'
-import Button from './Button'
 import User from '../assets/user.png'
+import { useState } from 'react'
+import Activity from './Activity'
 
 const Itinerary = ({name,by,img,price,hastags,duration,likes}) => {
+  const [viewActivity,setViewActivity] = useState(false);
+
   return (
     <div className='itinerary'>
         <div className='itinerary-name-container'>
@@ -38,7 +41,9 @@ const Itinerary = ({name,by,img,price,hastags,duration,likes}) => {
             }
         </div>
         <div className="itinerary-viewmore">
-            <Button content='View more' to='/' />
+            <button className="button" content='View more' onClick={()=>setViewActivity(!viewActivity)}>
+                {!viewActivity ? 'View more' : 'View less'}
+            </button>
         </div>
         <div className='itinerary-likes'>
             <span>{likes}</span>
@@ -46,6 +51,13 @@ const Itinerary = ({name,by,img,price,hastags,duration,likes}) => {
               <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
             </svg>
         </div>
+        {
+          viewActivity && 
+          <div className='activities'>
+            <Activity/>
+          </div>
+        }
+        
     </div>
   )
 }
